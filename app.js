0396d62443389063028e7d0214974fd1463fff27
Avaneesh Kumar <avaneeshk098@gmail.com>
2020-08-05 20:12:42 +0530

    Fixed Navbar Spacing

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 $(document).ready(function(){
 	var data;
-
-	$(".burger").click(function(){
-		$('#nav-links').toggleClass("show-nav");
-	});
-	$("#dropbtn1").click(function(){
-		$("#dropdown1").toggleClass("show1");
-		$("#dropdown2").removeClass("show2");
-		$("#dropdown3").removeClass("show3");
-	});
-	$("#dropbtn2").click(function(){
-		$("#dropdown1").removeClass("show1");
-		$("#dropdown2").toggleClass("show2");
-		$("#dropdown3").removeClass("show3");
-	});
-	$("#dropbtn3").click(function(){
-		$("#dropdown1").removeClass("show1");
-		$("#dropdown2").removeClass("show2");
-		$("#dropdown3").toggleClass("show3");
-	});
+	if(window.matchMedia("(max-width: 1200px)").matches){
+		$(".burger").click(function(){
+			$('#nav-links').toggleClass("show-nav");
+		});
+		$("#dropbtn1").click(function(){
+			$("#dropdown1").toggleClass("show1");
+			$("#dropdown2").removeClass("show2");
+			$("#dropdown3").removeClass("show3");
+		});
+		$("#dropbtn2").click(function(){
+			$("#dropdown1").removeClass("show1");
+			$("#dropdown2").toggleClass("show2");
+			$("#dropdown3").removeClass("show3");
+		});
+		$("#dropbtn3").click(function(){
+			$("#dropdown1").removeClass("show1");
+			$("#dropdown2").removeClass("show2");
+			$("#dropdown3").toggleClass("show3");
+		});
+	}
 
 	for(i = 0;i < document.getElementsByClassName("dropdown-content-in").length;i++){
 		document.getElementsByClassName("dropdown-content-in")[i].style.top =  i*48 + 'px';	
@@ -86,10 +87,13 @@ function generateHtmlTable(data) {
 			html += '<tr class="row">';
 			$.each(row, function( index, colData ) {
 				html += '<td class="data">';
-				if(index == 4){
+				if(index == 4 && colData !="#"){
 					html += '<a href="';
 					html += colData;
 					html += '"><button class="tlink">'+'Register Here'+'</button></a>';
+				}
+				else if(colData == "#"){
+					html+='<span>No Online Registration</span>'
 				}
 				else if(index == 5){
 					html += '<a href="';
